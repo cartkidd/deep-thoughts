3fd685a72050c65e8fc640d2138f0e3b33c9c227
Cartrell KIDD <cartrellk56@gmail.com>
2022-01-22 12:14:20 -0500

    error in syntax

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,11 @@
+const { User, Thought } = require('../models')
+
+
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
+      thoughts: async () => {
+        return Thought.find().sort({ createdAt: -1 });
       }
     }
   };
